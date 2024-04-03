c4c28d5797d7aa0d94ed14d8d28a9dde11b2d5ec
mofunemun <sinkamofunemu@gmail.com>
2024-04-03 23:45:21 +0900

    headerとfooter完成

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
-$(".header-button").click(function () {
-    $("body").toggleClass('body-blur');
+
+$('.header-button').click(function () {
+  $('.site-menu').addClass('show-site-menu');
 });
+
+$('.site-menu-cancel').click(function(){
+    $('.site-menu').removeClass('show-site-menu');
+  }
+);
+
